cd0382c8168978624fb2b9b4a5ed43d44146d482
Log22hey <heymann.logan@gmail.com>
2020-11-26 15:42:59 +0100

    creation des classes Customers, Products & Shopping

---
+++ DoudouMons/DoudouMons/DoudouMons.Core/models/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoudouMons.Core.models
+{
+    class Customers
+    {
+        [Key]
+        public int idCustomer { get; set; }
+        public String lastName { get; set; }
+        public String firstName { get; set; }
+        public String email { get; set; }
+        public String password { get; set; }
+        public String adresse { get; set; }
+        public int numero { get; set; }
+        public int cp { get; set; }
+        public String ville { get; set; }
+    }
+}
---
+++ DoudouMons/DoudouMons/DoudouMons.Core/models/Shopping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoudouMons.Core.models
+{
+    class Shopping
+    {
+        [Key]
+        public int idShopping { get; set; }
+        public int idCustomer { get; set; }
+        public int idProduct { get; set; }
+    }
+}
