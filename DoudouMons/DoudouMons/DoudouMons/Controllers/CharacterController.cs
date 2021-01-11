5e52ebeae001c5495bf6c4b2f5b56aa2fe3f5c50
Log22hey <heymann.logan@gmail.com>
2021-01-11 13:27:27 +0100

    modif

--- DoudouMons/DoudouMons/DoudouMons/Controllers/CharacterController.cs
+++ DoudouMons/DoudouMons/DoudouMons/Controllers/CharacterController.cs
@@ -36,6 +36,86 @@ namespace DoudouMons.Controllers
             return View(character);
         }
 
+        // GET: Character/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Character/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCharacter,Name,costume,ExplicationRole,NomArmes")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Characters.Add(character);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(character);
+        }
+
+        // GET: Character/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Character/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCharacter,Name,costume,ExplicationRole,NomArmes")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(character).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(character);
+        }
+
+        // GET: Character/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Character/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Character character = db.Characters.Find(id);
+            db.Characters.Remove(character);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
