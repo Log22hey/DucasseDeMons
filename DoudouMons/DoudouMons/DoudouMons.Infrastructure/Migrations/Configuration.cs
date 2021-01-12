f619a63e094226eac1091e083ddae5fdfefc25de
Log22hey <heymann.logan@gmail.com>
2021-01-12 09:23:02 +0100

    ajout de migrations

---
+++ DoudouMons/DoudouMons/DoudouMons.Infrastructure/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DoudouMons.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DoudouMons.Infrastructure.DAL.DoudouContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DoudouMons.Infrastructure.DAL.DoudouContext";
+        }
+
+        protected override void Seed(DoudouMons.Infrastructure.DAL.DoudouContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
